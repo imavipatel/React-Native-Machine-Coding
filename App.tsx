d63ccac4ebcae0c4e975db3572a2cbf7c5acfafb
Ankit Patel <ankit.patel4@aubank.in>
2025-12-18 12:08:52 +0530

    Changed App to main

--- App.tsx
+++ App.tsx
@@ -5,23 +5,46 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  Alert,
+  Button,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useEffect } from 'react';
 
-function App() {
+const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
+  useEffect(() => {
+    console.log('App component mounted');
+  }, []);
+
+  const onPressButton = () => {
+    console.log('hello');
+    console.log(isDarkMode);
+    Alert.alert('Button Pressed', `Dark mode: ${isDarkMode}`);
+  };
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container} edges={['top']}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+      <TouchableOpacity onPress={() => onPressButton()}>
+        <Text>Hello</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
